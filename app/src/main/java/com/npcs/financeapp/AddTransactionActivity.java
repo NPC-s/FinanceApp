13e2a6202bbc90f55751455e9d1ec0248bfa131b
Xa1era <IEBulatov.Ilya@gmail.com>
2023-11-25 01:03:15 +0500

    Работает вставка данных в бд. Рабоает преключение между страницами приложения

---
+++ app/src/main/java/com/npcs/financeapp/AddTransactionActivity.java
@@ -0,0 +1,53 @@
+package com.npcs.financeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddTransactionActivity extends AppCompatActivity {
+    private String[] categories = {"Продукты", "Развлечения", "Медицина", "Техника"};
+
+    private FinanceDBHandler DBHandler;
+
+    private EditText valueRaw;
+    private Switch isAddRaw;
+    private Spinner categoryTransRaw;
+    private Button confirm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        categoryTransRaw = findViewById(R.id.categoryOfTransaction);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoryTransRaw.setAdapter(adapter);
+
+        confirm = findViewById(R.id.BtnAddTransaction);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valueRaw = findViewById(R.id.idSetValue);
+                isAddRaw = findViewById(R.id.isAdd);
+
+                String value = valueRaw.getText().toString();
+                boolean isAdd = isAddRaw.getShowText();
+                String categoryTrans = categoryTransRaw.getSelectedItem().toString();
+
+                DBHandler = new FinanceDBHandler(AddTransactionActivity.this);
+                DBHandler.AddTransactions(value, isAdd, categoryTrans);
+                Intent intent = new Intent(AddTransactionActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/npcs/financeapp/FinancesDBSchemes.java
@@ -0,0 +1,15 @@
+package com.npcs.financeapp;
+
+import android.provider.BaseColumns;
+
+public class FinancesDBSchemes {
+
+    private FinancesDBSchemes() {}
+    public static class TransactionsHistory implements BaseColumns{
+        public static final String TABLE_NAME = "transactions";
+        public static final String COLUMN_NAME_VALUE = "value";
+        public static final String COLUMN_NAME_TYPE = "type";
+        public static final String COLUMN_NAME_CATEGORY = "category";
+    }
+
+}
